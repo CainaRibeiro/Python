dd81fdb0d71bedcdd7c8bea9e35b77d0d1275a04
CainaRibeiro <cainaribeiro.it@outlook.com>
2022-08-21 21:49:58 -0300

    Initial commit

---
+++ basics/variables.py
@@ -0,0 +1,51 @@
+print("No python não tem comando para adicionar uma variável");
+
+x = 5;
+
+print(x);
+
+print("As variáveis não precissam ser declaradas com algum type específico, o proprio python identifica o type da variável");
+
+n = 1;
+nome = 'cainã';
+media = 3.5;
+
+print(type(n));
+print(type(nome));
+print(type(media));
+
+print("Casting... Caso queira identificar um type específico para uma variável, basta utilizar o CASTING");
+
+n1 = float(1);
+name = str('caina');
+z = int(50);
+
+print(n1);
+print(name);
+print(z);
+
+print("Variáveis são case-sensitive");
+
+a = 1;
+A = 2;
+
+print(a);
+print(A);
+
+print('Você pode adicionar variás variáveis em uma única linha, economizando espaço no código');
+
+q, w, e = 'variavel 1', 'variável 2', 3;
+
+print(q);
+print(w);
+print(e);
+
+print('Você pode atribuir variáveis a itens de uma list ou tuple');
+
+frutas = ['maça', 'banana', 'mongo'];
+
+r, t, y = frutas;
+
+print(r);
+print(t);
+print(y);
