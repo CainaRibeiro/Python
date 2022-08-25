650da6cc2f064011a3aae55bb6344d1fea936752
Cainã Martins Teixeira Ribeiro <105864298+CainaRibeiro@users.noreply.github.com>
2022-08-25 09:31:18 -0300

    -

---
+++ testes/testes.py
@@ -0,0 +1,16 @@
+numero1, numero2 = int(input("Escolha um número: ")), int(input('Escolha outro número:'))
+
+
+def testeLogico(numero, idade):
+    if(numero > idade):
+        return True
+    else:
+        return False
+
+def funcaoTeste(numero, idade):
+    if testeLogico(numero, idade) == True:
+        print('Teste deu positivo')
+    else:
+        print('Teste deu merda')
+
+print(funcaoTeste(numero1, numero2))
